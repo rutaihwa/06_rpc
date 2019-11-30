a6d743c501517e226ec348e3c1ff5ec93d1d6226
Rutaihwa <rutaihwa@gmail.com>
2019-11-30 22:26:23 +0100

    Basic

--- jsonrpc-ring/src/main.rs
+++ jsonrpc-ring/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use failure::Error;
+use jsonrpc_http_server::jsonrpc_core::{IoHandler, Params, Value};
+use jsonrpc_http_server::ServerBuilder;
+use log::{debug, error, trace};
+use std::{env, net::SocketAddr};
+
+const START_ROLL_CALL: &str = "start_roll_call";
+const MARK_ITSELF: &str = "mark_itself";
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+
+    let addr: SocketAddr = env::var("ADDRESS")?.parse()?;
+
+    let mut io = IoHandler::new();
+    io.add_method("say_hello", |_params: Params| {
+        Ok(Value::String("hello".to_string()))
+    });
+
+    let server = ServerBuilder::new(io).threads(3).start_http(&addr)?;
+
+    Ok(server.wait())
 }
